858f6f23cc14bcd5edaf42b6e3f954607235562c
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-08-25 21:12:36 +0900

    update

--- others/scripts/prepare.ps1
+++ others/scripts/prepare.ps1
@@ -5,4 +5,4 @@ New-Item "./others/prepare/${filename}.ftml";
 ./SCP.exe page quote EN $name > $forumfile;
 Write-Output '**査読協力:** ' >> $forumfile;
 ./SCP.exe forum download-page EN $name >> $forumfile;
-Write-Output "Post('${name}', '', '')" >> ./post.ps1;
+Write-Output "Post('${name}', '', '');" >> ./post.ps1;
--- post.ps1
+++ post.ps1
@@ -19,4 +19,5 @@ $url = "fragment:apas-${i}";
 
 Post('he-who-screws-with-reality', '現実玩弄者である彼は', 'en tale 深淵目録');
 Post('note-your-name-is-nobody', 'メモ: あなたの名前は「何者でもない」', 'en goi-format 何者でもない _何者でもない 放浪者の図書館');
-Post('scp-4175', 'SCP-4175', 'en scp esoteric-class 深淵目録 シメリアン博士 倫理委員会 人間型 知性')
+Post('scp-4175', 'SCP-4175', 'en scp esoteric-class 深淵目録 シメリアン博士 倫理委員会 人間型 知性');
+Post('scp-4260', 'SCP-4260', 'en scp euclid 深淵目録 エントロピー 人間型 k-クラスシナリオ 現実改変 蘇生 管理者');
