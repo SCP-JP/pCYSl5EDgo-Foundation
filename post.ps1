311c03ea98fda43371d5eec4f3db3f8dfb08a5a8
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-08-25 21:22:04 +0900

    add

--- post.ps1
+++ post.ps1
@@ -21,3 +21,4 @@ Post('he-who-screws-with-reality', '現実玩弄者である彼は', 'en tale �
 Post('note-your-name-is-nobody', 'メモ: あなたの名前は「何者でもない」', 'en goi-format 何者でもない _何者でもない 放浪者の図書館');
 Post('scp-4175', 'SCP-4175', 'en scp esoteric-class 深淵目録 シメリアン博士 倫理委員会 人間型 知性');
 Post('scp-4260', 'SCP-4260', 'en scp euclid 深淵目録 エントロピー 人間型 k-クラスシナリオ 現実改変 蘇生 管理者');
+Post('scp-4855', 'SCP-4855', 'en 生命 認識災害 euclid 人間型 情報災害 ミーム 精神影響 神経 知性 scp 自我');
